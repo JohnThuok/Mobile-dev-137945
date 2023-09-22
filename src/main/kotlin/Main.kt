e8150c1083f4b4243a274456b4616b2e4b53976b
JohnThuok <john.kahar@strathmore.edu>
2023-09-22 10:02:16 +0300

    LAB 1 DONE

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,35 @@
-fun main(args: Array<String>) {
-    println ("What is your name?")
+/*fun main(args: Array<String>) {
+print ("Enter a number: ")
 
-    val name = readln()
-    println("Hello, $name")
+    val stringInput = readln()!!
+
+    var integer:Int = stringInput.toInt()
+
+    println("You entered: $integer")
+}*/
+
+/*fun main(args: Array<String>) {
+    val first: Int = 10
+    val second: Int = 20
+
+    val sum = first + second
+
+    println("the sum is: $sum")
+
+}*/
+
+/*fun main(args: Array<String>) {
+    val ch = 'i'
+
+    val vowelConstant = if (ch == 'a' ||ch == 'e' ||ch == 'i' ||ch == 'o' ||ch == 'u' )"vowel" else "constant"
+
+    println("$ch is $vowelConstant")
+}*/
+
+fun main(args: Array<String>) {
+    val ch = 'z'
+    when(ch){
+        'a','e','i','o','u' -> println("$ch is vowel")
+        else -> println ("$ch is consonant")
+    }
 }
